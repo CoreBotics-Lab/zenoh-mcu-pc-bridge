c9945f8bb4e68662841e3d09796778ed8aaebd0b
CoreBotics-Lab <hayisyed@gmail.com>
2026-07-29 13:22:07 +0530

    feat(mcu): implement setLed helper with brightness percentage scaling in RGB_ZenohRosSub

--- mcu_firmware_examples/RGB_ZenohRosSub/src/main.cpp
+++ mcu_firmware_examples/RGB_ZenohRosSub/src/main.cpp
@@ -7,6 +7,17 @@
 
 CRGB leds[NUM_LEDS];
 
+void setLed(int led, int r, int g, int b, int brightness){
+    //Convert brightness percentage to 0-255
+    brightness = (brightness * 255) / 100;
+
+    if(led < NUM_LEDS){ 
+    CRGB color = CRGB(r, g, b);
+    color.nscale8_video(brightness);
+    leds[led] = color;
+    FastLED.show();
+}
+}
 void setup() { 
 
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
@@ -14,12 +25,9 @@ void setup() {
 
 void loop() { 
   // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
-  FastLED.show();
+  setLed(0, 0, 255, 0, 5);
   delay(500);
   // Now turn the LED off, then pause
-  leds[0] = CRGB(255, 150, 255);
-  FastLED.show();
+  setLed(0, 0, 255, 0, 100);
   delay(500);
-
 }
