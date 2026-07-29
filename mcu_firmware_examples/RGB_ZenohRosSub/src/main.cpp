9390231aa76b74ac1d3d1c5f2cd482e2595b6537
CoreBotics-Lab <hayisyed@gmail.com>
2026-07-29 13:24:11 +0530

    removing unnecessary comments...

--- mcu_firmware_examples/RGB_ZenohRosSub/src/main.cpp
+++ mcu_firmware_examples/RGB_ZenohRosSub/src/main.cpp
@@ -7,27 +7,26 @@
 
 CRGB leds[NUM_LEDS];
 
-void setLed(int led, int r, int g, int b, int brightness){
-    //Convert brightness percentage to 0-255
-    brightness = (brightness * 255) / 100;
+void setLed(size_t led, uint8_t r, uint8_t g, uint8_t b, uint8_t brightness) {
+    if (led < NUM_LEDS) { 
+        uint8_t scaled_brightness = (brightness * 255) / 100;
+        
+        CRGB color = CRGB(r, g, b);
+        color.nscale8_video(scaled_brightness);
+        leds[led] = color;
+    }
+}
 
-    if(led < NUM_LEDS){ 
-    CRGB color = CRGB(r, g, b);
-    color.nscale8_video(brightness);
-    leds[led] = color;
-    FastLED.show();
-}
-}
 void setup() { 
 
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
 }
 
 void loop() { 
-  // Turn the LED on, then pause
   setLed(0, 0, 255, 0, 5);
+  FastLED.show();
   delay(500);
-  // Now turn the LED off, then pause
   setLed(0, 0, 255, 0, 100);
+  FastLED.show();
   delay(500);
 }
