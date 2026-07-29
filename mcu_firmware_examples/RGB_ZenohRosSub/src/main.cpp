334ab9d7b2eee09bca3946611784f76d04bcf289
CoreBotics-Lab <hayisyed@gmail.com>
2026-07-29 22:01:35 +0530

    test(mcu): verify rainbowCycle animation on external 16-LED WS2812B ring

--- mcu_firmware_examples/RGB_ZenohRosSub/src/main.cpp
+++ mcu_firmware_examples/RGB_ZenohRosSub/src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define NUM_LEDS 1
+#define NUM_LEDS 16
 
-#define DATA_PIN 48
+#define DATA_PIN 14
 
 CRGB leds[NUM_LEDS];
 
@@ -17,16 +17,28 @@ void setLed(size_t led, uint8_t r, uint8_t g, uint8_t b, uint8_t brightness) {
     }
 }
 
+void rainbowCycle(uint8_t brightness, uint8_t wait) {
+    static uint16_t j = 0;
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CHSV((i * 256 / NUM_LEDS + j) & 255, 255, ((brightness * 255) / 100));
+        // leds[i].nscale8_video((brightness * 255) / 100);
+    }
+    FastLED.show();
+    delay(wait);
+    j++;
+}
+
 void setup() { 
 
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
 }
 
 void loop() { 
-  setLed(0, 0, 255, 0, 5);
-  FastLED.show();
-  delay(500);
-  setLed(0, 0, 255, 0, 100);
-  FastLED.show();
-  delay(500);
+//   setLed(0, 0, 255, 0, 5);
+//   FastLED.show();
+//   delay(500);
+//   setLed(0, 0, 255, 0, 100);
+//   FastLED.show();
+//   delay(500);
+    rainbowCycle(25, 1);
 }
